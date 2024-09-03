4853fefeae2afa5e8320c04939ac582a3333d868
Thiwanga Jayasinghe <136949635+Thiwanga123@users.noreply.github.com>
2024-09-03 16:30:23 +0000

    added member class

---
+++ Member.java
@@ -0,0 +1,13 @@
+public class Member {
+    String membreID;
+    String address;
+    String name;
+    String phoneNumber;
+
+    Member(String memberID,String name, String address, String phonenumber){
+        this.membreID=memberID;
+        this.name=name;
+        this.address=address;
+        this.phoneNumber=phonenumber;
+    }
+}
